e116edfdb5a479881c41affcf68b69b4836cef76
e-b11 <96320276+e-b11@users.noreply.github.com>
2023-11-12 22:51:49 -0700

    images

--- hikingapp/index.js
+++ hikingapp/index.js
@@ -4,16 +4,26 @@ let app = express();
 
 let path = require("path");
 
+let favicon = require("serve-favicon");
+
+//Set port
 const port = 3000;
 
+//Add ejs
 app.set("view engine", "ejs");
+
+//favicon
+app.use(favicon(__dirname + "/public/images/favicon.ico"));
 
 app.use(express.urlencoded({ extended: true }));
 
+//Connect css file
 app.use(express.static(__dirname + "/public"));
 
+//Render index.ejs as home page
 app.get("/", (req, res) => {
   res.render(path.join(__dirname + "/views/index.ejs"));
 });
 
+//Tell it what port to listen out and send a message when it starts
 app.listen(port, () => console.log("Website started."));
