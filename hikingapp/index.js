a1982c3b658bd2ba53f760dd2066804b2556d230
e-b11 <96320276+e-b11@users.noreply.github.com>
2023-12-12 14:14:13 -0700

    routes/packages

--- hikingapp/index.js
+++ hikingapp/index.js
@@ -1,24 +1,39 @@
-const express = require("express");
+//Project 3: Hiking Tracker
+// Emma Bastian, Connor Humphrey, Johnny Fietkau, Lauren do Lago
 
-let app = express();
+//Express Package
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const favicon = require("serve-favicon");
 
 let path = require("path");
-
-let favicon = require("serve-favicon");
+const faviconPath = path.join(__dirname, "public");
+let app = express();
 
 //Set port
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //Add ejs
 app.set("view engine", "ejs");
-
-//favicon
-app.use(favicon(__dirname + "/public/images/favicon.ico"));
-
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 //Connect css file
 app.use(express.static(__dirname + "/public"));
+app.use(favicon(path.join(faviconPath, "favicon.ico")));
+
+// Set up knex, will have to adjust database
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: process.env.RDS_HOSTNAME || "localhost",
+    user: process.env.RDS_USERNAME || "postgres",
+    password: process.env.RDS_PASSWORD || "postgres",
+    database: process.env.RDS_DB_NAME || "intex",
+    port: process.env.RDS_PORT || 5432,
+    ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
+  },
+});
 
 //Render index.ejs as home page
 app.get("/", (req, res) => {
